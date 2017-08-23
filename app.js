be27e3425e51b072a2e1f438cb4f9e438a626cca
Peter Fessel <peter.fessel@live.de>
2017-08-23 15:01:17 +0100

    implement getWordsForCurrentIteration method to handle word selection edge cases

--- app.js
+++ app.js
@@ -18,29 +18,26 @@ window.app = (function () {
   function run (textInput) {
     // var uniqueWordArray = generateUniqueWordArray(textInput),
     //     wordArray = splitTextInputIntoWords(textInput),
-    //     numberOfInputs = 2,
+    //     numberOfInputs = 3,
     //     numberOfOutputs = uniqueWordArray.length,
     //     numberOfHiddenNeurons = uniqueWordArray.length / 2, // experiment with this
     //     numberOfHiddenLayers = 1, // usually the best default
     //     weightRange = [-0.5, 0.5],
     //     network = initialiseNeuronalNetwork (numberOfInputs, numberOfOutputs, numberOfHiddenNeurons, numberOfHiddenLayers, weightRange);
-    console.log(splitTextInputIntoWords(textInput));
-    // train network
-    // for (var i = 0; i < splitTextInputIntoWords.length; i++) {
-    //   trainNetwork(network, wordArray, numberOfInputs);
-    // }
+    var wordArray = splitTextInputIntoWords(textInput),
+        network = [],
+        numberOfInputs = 3;
+    // train network; start by selecting the first word (i = 1)
+    for (var i = 1; i <= wordArray.length; i++) {
+      var wordInput = getWordsForCurrentIteration(wordArray, numberOfInputs, i);
+      trainNetwork(network, wordInput);
+    }
   }
 
   /* Initialises the neuronal network.
      Returns an array of arrays where the latter represent the network layers. */
   function initialiseNeuronalNetwork (numberOfInputs, numberOfOutputs, numberOfHiddenNeurons, numberOfHiddenLayers, weightRange) {
 
-  }
-
-  /* Splits a string into words with periods occuring as words of their own, other punctuation marks removed.
-   * Returns an array of strings. */
-  function splitTextInputIntoWords (textInput) {
-    return textInput.split(/[\s\-]+/);
   }
 
   /*
@@ -50,9 +47,47 @@ window.app = (function () {
    *  - networkLayers: an array of network layers (again arrays) which will be changed as part of the training process (side effects)
    *  - wordArray: an array of the text input with periods occuring as words of their own, other punctuation marks removed
    *  - numberOfInputs: the number of words that should be considered to train and trigger the network
+   *  - iteration: the training iteration/epoch; equals the word position in wordArray that the epoch is focused on
    */
-  function trainNetwork (networkLayers, wordArray, numberOfInputs) {
+  function trainNetwork (networkLayers, wordInput) {
+    console.log(wordInput);
+  }
 
+  /*
+   * Returns an array of w words out of an array with i being the last word of the array and w words preceding the last word.
+   * Handles the following edge cases:
+   *  - fills up the returned array with empty words (empty strings) in case (i - w) is smaller than zero
+   *  - splits punctuation characters at the end of words into words of its own
+   *
+   * Returns an array of strings.
+   */
+  function getWordsForCurrentIteration (wordArray, w, i) {
+    var result = [];
+
+    // handle edge case when i is at the beginning of wordArray
+    if (i <= w) {
+      // fill up non-existent words with empty strings
+      for (var c = 0; c < w - i; c++) {
+        result.push("");
+      }
+      // select words from wordArray for words that do exist in wordArray
+      for (var c = 0; c < i ; c++) {
+        result.push(wordArray[c]);
+      }
+    } // normal slicing
+    else {
+      result = wordArray.slice(i - w, i);
+    }
+
+    return result;
+  }
+
+  /* Splits a string into words with punctuation marks occuring on their own.
+   * Returns an array of strings. */
+  function splitTextInputIntoWords (textInput) {
+    return textInput.split(/\b/).filter(function (word) {
+      return word.match(/\S/); // filter out whitespace
+    });
   }
 
   return {
